f99413cc12fb0b6549d6488e95c86521eb104f29
dark-wizz <mailwithakram@gmail.com>
2025-01-14 04:33:55 +0530

    Alias fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 # akram
+set -x HYPRSHOT_DIR /home/akram/Pictures/screenshots
+set -x EDITOR nvim
+
+alias cd z
+alias S "sudo pacman -S"
+alias R "sudo pacman -S"
+
 zoxide init fish | source
